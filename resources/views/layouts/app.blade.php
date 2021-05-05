dd00df2e04f1d5c5d8a84f619d60130c2170df11
amnayottama <shadowandhika@gmail.com>
2021-05-05 22:55:01 +0700

    nambah migrate dan ui.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,12 +40,9 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
